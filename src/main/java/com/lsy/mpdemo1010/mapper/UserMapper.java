99d22da34a332b7a5f13d544477f1ac22c81c727
just_gol <809073023@qq.com>
2020-04-05 17:26:14 +0800

    mybatis-plus demo

---
+++ src/main/java/com/lsy/mpdemo1010/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.lsy.mpdemo1010.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lsy.mpdemo1010.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface UserMapper extends BaseMapper<User> {
+
+}
